a7f28bd50db6d42f9da68518ae63dddfa75a9aa5
Amber955 <149734997+Amber955@users.noreply.github.com>
2024-01-18 15:22:53 +0100

    Create katy.js

---
+++ challenge1/katy.js
@@ -0,0 +1,47 @@
+background(37, 150, 190);
+
+//body
+fill(242, 240, 223);
+ellipse(262, 250, 220, 200);
+
+//face
+fill(242, 240, 223);
+stroke(0, 0, 0);
+ellipse(255, 130, 150, 125);
+
+//the eyes
+fill(0, 0, 0);
+ellipse(231, 112, 20, 20); //the right eye
+ellipse(272, 112, 20, 20); //the left eye
+
+//pupils
+fill(255, 255, 255);
+ellipse(236, 112, 5, 7); //the right pupil
+ellipse(277, 112, 5, 7); //the left pupil
+
+//the nose
+fill(0, 0, 0);
+ellipse(253, 133, 10, 7);
+
+//cats mustaches hehe!
+fill(0, 0, 0);
+line(270, 143, 292, 135);
+line(270, 147, 296, 147);
+line(239, 146, 215, 140);
+line(239, 149, 211, 149);
+
+//the cats ears
+fill(0, 0, 0);
+triangle(268, 69, 294, 77, 301, 35); //the right ear
+triangle(225, 74, 204, 85, 194, 38); //the left ear
+
+//the tail
+fill(242, 240, 223);
+rect(68, 318, 120, 20, 9);
+
+//the legs
+fill(0, 0, 0);
+ellipse(188, 317, 30, 30);
+ellipse(221, 336, 30, 30);
+ellipse(353, 275, 30, 30);
+ellipse(330, 311, 30, 30);
